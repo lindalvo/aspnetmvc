37212a77ef14ca39f476d50855a8dd7012671b39
Lindalvo Neto <lindalvo@outlook.com>
2019-11-02 11:45:35 -0300

    Conclusão do ASNET MVC5. continuar ASPNET MVS Core

---
+++ asnetmvc5/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Filtros;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    [AutorizacaoFilter]
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        [Route("produtos",Name ="ListaProdutos")]
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList <Produto> produtos = dao.Lista();
+            return View(produtos);
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDAO = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido","Informática com preço abaixo de cem reais");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+            }
+        }
+        [Route("produtos/{id}",Name ="VisualizaProduto")]
+        public ActionResult Visualiza(int Id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(Id);
+            ViewBag.Produto = produto;
+            return View();
+        }
+
+        public ActionResult DecrementaQtd(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            produto.Quantidade--;
+            dao.Atualiza(produto);
+            return Json(produto);
+        }
+    }
+}
